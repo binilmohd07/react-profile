443230e55fce46d5401a23702aaa3d11639ad869
binilmohamed07 <binilmohamed07@gmail.com>
2025-04-28 21:32:49 +0530

    Test cases updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Name check', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Binil Mohamed Nizar/i);
   expect(linkElement).toBeInTheDocument();
 });
